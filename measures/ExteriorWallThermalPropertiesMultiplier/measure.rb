2fababed98fe41e7f55cd71324a62fb3a4a51efb
Brian Ball <Brian.Ball@nrel.gov>
2017-07-06 10:00:23 -0600

    rename and clean up exterior wall measure

--- measures/ExteriorWallThermalPropertiesMultiplier/measure.rb
+++ measures/ExteriorWallThermalPropertiesMultiplier/measure.rb
@@ -3,9 +3,25 @@ class ChangeExteriorWallThermalProperties < OpenStudio::Ruleset::ModelUserScript
 
   #define the name that a user will see
   def name
-    return "Change Exterior Wall Thermal Properties"
+    return "Exterior Wall Thermal Properties Multiplier"
   end
 
+  #short def to make numbers pretty (converts 4125001.25641 to 4,125,001.26 or 4,125,001). The definition be called through this measure
+  def neat_numbers(number, roundto = 2) #round to 0 or 2)
+    if roundto == 2
+      number = sprintf "%.2f", number
+    else
+      number = number.round
+    end
+    #regex to add commas
+    number.to_s.reverse.gsub(%r{([0-9]{3}(?=([0-9])))}, "\\1,").reverse
+  end #end def neat_numbers
+
+  #helper to make it easier to do unit conversions on the fly
+  def unit_helper(number,from_unit_string,to_unit_string)
+    OpenStudio::convert(OpenStudio::Quantity.new(number, OpenStudio::createUnit(from_unit_string).get), OpenStudio::createUnit(to_unit_string).get).get.value
+  end
+    
   #define the arguments that the user will input
   def arguments(model)
     args = OpenStudio::Ruleset::OSArgumentVector.new
@@ -42,39 +58,6 @@ class ChangeExteriorWallThermalProperties < OpenStudio::Ruleset::ModelUserScript
     r_value_mult = runner.getDoubleArgumentValue("r_value_mult",user_arguments)
     solar_abs_mult = runner.getDoubleArgumentValue("solar_abs_mult",user_arguments)
     thermal_mass_mult = runner.getDoubleArgumentValue("thermal_mass_mult",user_arguments)
-
-    #check the input arguments for percentage input or problomatically small values
-    arg_in = [r_value_mult, solar_abs_mult, thermal_mass_mult]
-    arg_in_d = ["R-value multiplier","Solar absorptance multiplier", "Thermal mass multiplier"]
-    arg_flag = FALSE
-    arg_in.each_with_index do |arg, arg_index|
-      if arg.round(2) == 0
-        runner.registerError("#{arg_in_d[arg_index]} was set equal to #{arg}. Please input a value greater that 1*e-2.")
-        arg_flag = TRUE
-      elsif arg < 0
-        runner.registerError("#{arg_in_d[arg_index]} was set equal to #{arg}. Please input a number greater than zero. Remember a multiplier, not a percentage, is being specified.")
-        arg_flag = TRUE
-      elsif arg >= 3
-        runner.registerWarning("#{arg_in_d[arg_index]} was set equal to #{arg}. Please ensure that the desired value was entered as a multiplier, not a percentage.")
-      end
-    end
-    return false if arg_flag
-
-    #short def to make numbers pretty (converts 4125001.25641 to 4,125,001.26 or 4,125,001). The definition be called through this measure
-    def neat_numbers(number, roundto = 2) #round to 0 or 2)
-      if roundto == 2
-        number = sprintf "%.2f", number
-      else
-        number = number.round
-      end
-      #regex to add commas
-      number.to_s.reverse.gsub(%r{([0-9]{3}(?=([0-9])))}, "\\1,").reverse
-    end #end def neat_numbers
-
-    #helper to make it easier to do unit conversions on the fly
-    def unit_helper(number,from_unit_string,to_unit_string)
-      OpenStudio::convert(OpenStudio::Quantity.new(number, OpenStudio::createUnit(from_unit_string).get), OpenStudio::createUnit(to_unit_string).get).get.value
-    end
 
     #create an array of exterior surfaces and construction types
     surfaces = model.getSurfaces
@@ -176,6 +159,7 @@ class ChangeExteriorWallThermalProperties < OpenStudio::Ruleset::ModelUserScript
       #create and name new construction
       new_construction = construction.clone
       new_construction = new_construction.to_Construction.get
+      #TODO change name to x change
       new_construction.setName("Calibrated Exterior #{construction.name.to_s}")
       #replace layers in new construction
       new_construction.layers.each_with_index do |layer, lay_index|
@@ -185,6 +169,7 @@ class ChangeExteriorWallThermalProperties < OpenStudio::Ruleset::ModelUserScript
         new_layer.to_StandardOpaqueMaterial.get.setSolarAbsorptance(desired_sol_abs[con_index]) if lay_index == 0 && layer.to_StandardOpaqueMaterial.is_initialized #only apply to outer surface
         new_layer.to_OpaqueMaterial.get.setThermalResistance(desired_r_val[con_index][lay_index]) if layer.to_OpaqueMaterial.is_initialized
         new_layer.to_StandardOpaqueMaterial.get.setDensity(desired_thm_mass[con_index][lay_index]) if layer.to_StandardOpaqueMaterial.is_initialized && desired_thm_mass[con_index][lay_index] != 0
+        #TODO change name to x change
         new_layer.setName("#{new_layer.name.to_s} in #{new_construction.name.to_s}")
         new_construction.setLayer(lay_index, new_layer)
         #calculate properties of new layer and output nice names
@@ -194,6 +179,7 @@ class ChangeExteriorWallThermalProperties < OpenStudio::Ruleset::ModelUserScript
         final_r_val_d[con_index][lay_index] = neat_numbers(final_r_val[con_index][lay_index])
         final_sol_abs_d[con_index] = neat_numbers(final_sol_abs[con_index]) if lay_index == 0 && layer.to_StandardOpaqueMaterial.is_initialized
         final_thm_mass_d[con_index][lay_index] = neat_numbers(final_thm_mass[con_index][lay_index]) if layer.to_StandardOpaqueMaterial.is_initialized
+        #TODO change the naming and info
         runner.registerInfo("Updated material '#{layer.name.to_s}' in construction '#{construction.name.to_s}' to '#{new_layer.name.to_s}' as follows:")
         final_r_val[con_index][lay_index] ? runner.registerInfo(" R-Value updated from #{initial_r_val_d[con_index][lay_index]} to #{final_r_val_d[con_index][lay_index]} (#{final_r_val[con_index][lay_index]/initial_r_val[con_index][lay_index]} mult)") : runner.registerInfo("R-Value was #{initial_r_val_d[con_index][lay_index]} and now is nil_value")
         final_thm_mass[con_index][lay_index] ? runner.registerInfo("Thermal Mass updated from #{initial_thm_mass_d[con_index][lay_index]} to #{final_thm_mass_d[con_index][lay_index]} (#{final_thm_mass[con_index][lay_index]/initial_thm_mass[con_index][lay_index]} mult)") : runner.registerInfo("Thermal Mass was #{initial_thm_mass[con_index][lay_index]} and now is nil_value")
@@ -207,7 +193,7 @@ class ChangeExteriorWallThermalProperties < OpenStudio::Ruleset::ModelUserScript
         surface.setConstruction(new_construction) if surface.construction.get.handle.to_s == construction.handle.to_s
       end
     end
-
+#TODO remove this
     #create an array of exterior surfaces and construction types
     final_surfaces = model.getSurfaces
     final_exterior_surfaces = []
